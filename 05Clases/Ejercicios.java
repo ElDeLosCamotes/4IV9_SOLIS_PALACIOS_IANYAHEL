bb56adcf7759a147b495219a3b9a26873c9849f2
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-04-13 17:42:20 -0600

    Clases

--- 05Clases/Ejercicios.java
+++ 05Clases/Ejercicios.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class Ejercicios {
-    
+    Scanner entrada = new Scanner(System.in);
+    double num1 = 0;
+    double num2 = 0;
+    double num3 = 0;
+    public void menu(){
+        System.out.println("Bienvenido a la calculadora");
+        System.out.println("Elige una opcion");
+        System.out.println("1.- Suma ");
+        System.out.println("2.- Resta ");
+        System.out.println("3.- Multiplicacion ");
+        System.out.println("4.- Salir ");
+
+        int opcion = 0;
+        opcion = entrada.nextInt();
+
+        switch (opcion) {
+            case 1 :
+                System.out.println("Ingresa el primer numero");
+                num1 = entrada.nextDouble();
+                System.out.println("Ingresa el segundo numero");
+                num2 = entrada.nextDouble();
+                num3= num1 + num2;
+                
+                System.out.println("La suma de los numeros es " + num3);
+                break;
+            case 2 :
+            break;
+        }
+
+
+    }
 }
