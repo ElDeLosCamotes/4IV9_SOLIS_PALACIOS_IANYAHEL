b84638c04e8f8bbbc8a32bb60273351242c1a9bf
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-04-27 16:28:41 -0600

    Sobrecarga

---
+++ 08Herencia/Animal.java
@@ -0,0 +1,21 @@
+//La herencia es la capacidad de poder tener acceso a los atributos y conportaminetos de una clase padre a una clase hijo
+
+//El encapsulamiento es la capacidad de podert restringir el acceso a los tipos de datos, comportamientos u atributoa de una clase frente a otra. Con ello vamos a poder agrupar en una clase el accespo a los metodos, atributos y objetos para que no puedan ser modificados.
+
+//sobrecarga 
+public class Animal {
+
+    private String nombre, raza, tipo_alimento;
+    private int edad;
+
+    //toda clase debe de tener un constructor
+    //Los constructores sirven para poder declarar la existencia de una clase, para poder inicializar sus variables y poder crear instancias de la misma.
+
+    public Animal(){
+        // su acceso es publico
+        // el nombbre del constructor es el mismo que el de la clase pero sin parametros.
+        //Es un metodo
+    }
+
+
+}
