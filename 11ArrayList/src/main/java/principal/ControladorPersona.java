5491ea53598b9a323bf79e474322c1757fc2f9bd
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-05-12 20:19:19 -0600

    Persona 5 Royal

---
+++ 11ArrayList/src/main/java/principal/ControladorPersona.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+/**
+ *
+ * @author Alumno
+ */
+
+import java.util.ArrayList;
+
+public class ControladorPersona {
+    
+    /*
+    Vamos a ocupar un objeto especial para poder almacenar diferentes tipos de varibales y diferentes onjetos llamos arraylist, su particularidad es redimencional
+    */
+    
+    
+}
