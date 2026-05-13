5770e0df3b3ee20eb79a6658af4969a472d77c04
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-05-12 20:31:28 -0600

    Vista Persona

--- 11ArrayList/src/main/java/principal/ControladorPersona.java
+++ 11ArrayList/src/main/java/principal/ControladorPersona.java
@@ -13,9 +13,46 @@ import java.util.ArrayList;
 
 public class ControladorPersona {
     
-    /*
-    Vamos a ocupar un objeto especial para poder almacenar diferentes tipos de varibales y diferentes onjetos llamos arraylist, su particularidad es redimencional
-    */
-    
-    
+      /*
+        Vamos a crear un objeto especialk para poder almacenar diferentes tipos de variables, y diferenciatr objetos, llamado arraylist, su particularidad es rendimiento
+        */
+        public static ArrayList<Persona> listadePersonas = new ArrayList<Persona>();
+       
+        //el metodo
+        public ArrayList<Persona> mostrarPersonas(){
+            return listadePersonas;
+        }
+       
+        //registrar
+       
+        public void registrarPersona(Persona p){
+            listadePersonas.add(p);
+        }
+        //buscar persona
+       
+        public Persona buscarPersona (int id){
+            Persona encontrada = new Persona();
+            //recorro el arraylist
+           
+            for(Persona p: listadePersonas){
+                if(id == p.getId()){
+                encontrada = p;
+               
+                }else{
+                    System.out.println("Solo juego contigo");
+                }
+            }
+            return encontrada;
+        }
+       
+        public void actualizarPersona(Persona personaActualizada){
+            Persona personaActualizar = buscarPersona(personaActualizada.getId());
+            listadePersonas.remove(personaActualizar);
+            listadePersonas.add(personaActualizar);
+        }
+       
+        public void eliminarPersona(Persona personaEliminar){
+            listadePersonas.remove(personaEliminar);
+        }
 }
+
