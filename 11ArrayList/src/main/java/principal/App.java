4f7ca5ea1b44adb4098a0ddacc6a012dcd4d6aed
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-05-12 20:51:34 -0600

    Persona 5  Royal

--- 11ArrayList/src/main/java/principal/App.java
+++ 11ArrayList/src/main/java/principal/App.java
@@ -11,6 +11,8 @@ package principal;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+        VistaPersona ventana = new VistaPersona();
+        ventana.Principal();
     }
 }
