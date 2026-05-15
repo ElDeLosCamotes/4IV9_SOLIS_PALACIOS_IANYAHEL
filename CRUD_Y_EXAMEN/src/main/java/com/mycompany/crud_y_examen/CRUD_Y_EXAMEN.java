9359d5b9e3476cd1be29ed8225adc6361d87971a
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-05-14 20:27:04 -0600

    Array

---
+++ CRUD_Y_EXAMEN/src/main/java/com/mycompany/crud_y_examen/CRUD_Y_EXAMEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud_y_examen;
+
+/**
+ *
+ * @author IanYa
+ */
+public class CRUD_Y_EXAMEN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
