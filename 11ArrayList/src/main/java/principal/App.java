6d2edc08a867d3b96563805ce3362e887f5a1490
ElDeLosCamotes <isolisp2400@alumno.ipn.mx>
2026-05-14 22:07:51 -0600

    Actualizar

--- 11ArrayList/src/main/java/principal/App.java
+++ 11ArrayList/src/main/java/principal/App.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package principal;
 
 /**
  *
- * @author Alumno
+ * @author demon
  */
 public class App {
 
     public static void main(String[] args) {
-    
+        System.out.println("Hello World!");
+        
         VistaPersona ventana = new VistaPersona();
         ventana.Principal();
     }
---
+++ herenciapersona/src/main/java/principal/herenciapersona/DAOEstudiante.java
@@ -0,0 +1,121 @@
+
+package principal.herenciapersona;
+
+
+import javax.swing.JOptionPane;
+
+public class DAOEstudiante {
+    
+    //vamos a crear un programa para dar de alta 10 estudiantes
+    
+    //un objeto por parte de estudiante
+    Estudiante obj[] = new Estudiante[10];
+    int x = 0;
+    
+    //metodo del menu para el programa
+    void menu(){
+        String var = "si";
+        String mensaje = "";
+        
+        while(var.equalsIgnoreCase("si")){
+            int op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Ingresa la opcion deseada : \n"
+                    + "1.- Dar de alta a nuevo estudiante. \n"
+                    + "2.- Mostrar los datos de todos los estudiantes \n"
+                    + "3.- Eliminar Datos de un Estudiante \n"
+                    + "4.- Actualizar Estudiante \n"
+                    ));
+            switch (op) {
+                case 1:
+                    //metodo para registrar
+                    pedirEstudiante();                    
+                    
+                    break;
+                case 2:
+                    //mostrar estudiantes
+                    mostrarEstudiante();
+                    
+                    break;
+                    
+                case 3:
+                    eliminarEstudiante();
+                    
+                    break;
+                
+                case 4:
+                    actualizarEstudiante();
+                    
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+            mensaje = JOptionPane.showInputDialog("¿Desea repetir el programa?");
+            var = mensaje;
+        }
+    }
+
+    public void pedirEstudiante() {
+        if (x >=obj.length) {
+            JOptionPane.showMessageDialog(null, "Ya no se pueden registrar mas estudiantes");
+        }
+        
+        int numboleta = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Ingresa la boleta del estudiante: "));
+        String nom = JOptionPane.showInputDialog(
+                "Ingresa el nombre del estudiante");
+        int edad = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Ingresa la edad del estudiante: "));
+        char gen = JOptionPane.showInputDialog(
+                "Ingresa el genero del estudiante").charAt(0);
+        
+        obj[x] = new Estudiante(numboleta, nom, edad, gen);
+        x++;
+        
+        
+    }
+
+    public void mostrarEstudiante() {
+        //necesitamos recorrer el arreglo del tamaño que sea
+        
+        for(int i = 0; i < x; i++){
+            //visualizar los datos
+            JOptionPane.showMessageDialog(null, 
+                    "La boleta del estudiante es: " + obj[i].getNumBoleta() + "\n"
+                    +"El nombre del estudiante es: " + obj[i].getNombre()+ "\n"
+                   +"La edad del estudiante es: " + obj[i].getEdad() + "\n"
+                   +"El genero del estudiante es: " + obj[i].getGenero()+ "\n");
+        }
+    }
+    
+    public void eliminarEstudiante (){
+        
+    }
+    
+    public void actualizarEstudiante (){
+        int boletaBuscar = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero de boleta del estudiante a actualizar"));
+        boolean encontrado = false;
+        for(int i = 0; i < x; i++){
+            if (obj[i] !=null && obj[i].getNumBoleta() == boletaBuscar){
+                //Pedir los nuevos datos jeje
+                int nuevaBoleta = Integer.parseInt(JOptionPane.showInputDialog("Nueva boleta (actual: " + obj[i].getNumBoleta()+ "):"));
+                String nuevoNom = JOptionPane.showInputDialog("Nuevo nombre (actual: " + obj[i].getNombre()+ "):");
+                int nuevaEdad = Integer.parseInt(JOptionPane.showInputDialog("Nueva edad (actual: " + obj[i].getEdad()+ "):"));
+                char nuevoGen = JOptionPane.showInputDialog("Nueva boleta (actual: " + obj[i].getGenero()+ "):").charAt(0);
+                
+                //creamos un nuevo objeto pa guardarlo
+                obj[i] = new Estudiante (nuevaBoleta,nuevoNom, nuevaEdad, nuevoGen);
+                
+                JOptionPane.showMessageDialog(null, "Estudiante actualizado");
+                encontrado = true;
+                break;
+            }
+        }
+        if(!encontrado){
+            
+            JOptionPane.showMessageDialog(null, "Estudiante no Encontrado");
+        }
+    }
+    
+}
